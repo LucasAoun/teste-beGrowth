07e5567e1b9fd7d967e21aa1273aa3904838d5c4
Lucas Aoun <souolucas2009@gmail.com>
2020-11-19 13:37:08 -0300

    pagination created

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,11 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import '../styles/pages/home.css'
 import Sidebar from '../components/Sidebar'
-import roxo from '../assets/roxo.jpg'
 import api from '../services/api'
-
-
-
 
 
 interface Item {
@@ -18,14 +14,36 @@ interface Item {
 
 function Home (){
 
-
     const [photos, setPhotos] = useState<Item[]>([])
+    const [loading, setLoading] = useState(false)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [photosPerPage] = useState(50)
+
 
     useEffect(()=>{
+            setLoading(true)
         api.get('photos').then(response=>{
             setPhotos(response.data)
+            setLoading(false)
           })
     },[])
+
+    if(loading){
+        return <h1>Loading...</h1>
+    }
+
+
+
+    const indexOfLastPhoto = currentPage * photosPerPage
+    const indexOfFirstPhoto = indexOfLastPhoto - photosPerPage
+    const currentPhotos = photos.slice(indexOfFirstPhoto, indexOfLastPhoto)
+
+    const pageNumbers = []
+    for(let i = 1; i<=Math.ceil(photos.length / photosPerPage); i++){
+        pageNumbers.push(i)
+    }
+
+    const paginate = (pageNumber:any) => setCurrentPage(pageNumber)
 
  return(
      <div className="page-home">
@@ -39,13 +57,21 @@ function Home (){
              </nav>
              
              <div className="card-container">
-                 {photos.map(item=>(
-                <div className="card">
+                 {currentPhotos.map(item=>(
+                <div key={item.id} className="card">
                     <img src={item.thumbnailUrl} alt=""/>
                     <p className="thumbTitle">{item.title}</p>
                 </div>
                 ))}
-                
+                 <ul className="pagination">
+                {pageNumbers.map(number=>(
+                    <li className="page-item">
+                        <a id="page-link" href="#" onClick={()=>paginate(number)} className="page-link">
+                            <p>{number}</p>
+                        </a>
+                    </li>
+                ))}
+            </ul>
             </div>
         </div>
      </div>
